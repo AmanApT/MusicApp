dc3e980a68e3143434285dca3809ff8dff30bb10
AmanApT <amanasstudent@gmail.com>
2023-10-08 15:48:42 +0530

    Background Clr

--- App.js
+++ App.js
@@ -5,28 +5,21 @@ import { LinearGradient } from "expo-linear-gradient";
 export default function App() {
   return (
     <View style={styles.container}>
-      <LinearGradient
-        colors={[
-          "#2E013F",
-          "rgba(8, 0, 26, 0.85)",
-          "rgba(0, 0, 0, 0.95)",
-          "#28164F",
-        ]}
-        style={styles.background}
-      >
+      <LinearGradient colors={["#22004e", "#000"]} style={styles.background}>
         {/* <StatusBar style="auto" color="white" /> */}
-        <Text>Heeellu World</Text>
+        <Text>Music App</Text>
         {/* <StatusBar style="auto" /> */}
       </LinearGradient>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
   background: {
     flex: 1,
