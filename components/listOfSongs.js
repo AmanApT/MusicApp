08a8d14f215ef57aad6e6aca6c4f69d5478822d1
Anupreet Srivastava <anupreet.2003@gmail.com>
2023-10-10 20:14:09 +0530

    Image path daal ke de

---
+++ components/listOfSongs.js
@@ -0,0 +1,11 @@
+const songs = [
+  {
+    id: 1,
+    title: "Apna Bana Le",
+    artist: "Arijit Singh",
+    imagePath: require(""),
+    songPath: "require('./assets/coverSong.mp3')",
+  },
+];
+
+export default songs;
